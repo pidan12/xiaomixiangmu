b97efe5fa93ef4cc0b2dd8d4f24fc0d251335c5d
pidan12 <2053929477@qq.com>
2018-03-12 19:24:44 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//轮播效果
 {
     let imgs=document.querySelectorAll(".banner_img li");
     let pagers=document.querySelectorAll(".banner_lunbo li");
@@ -104,7 +105,7 @@
         for(let i=0;i<cons.length;i++){
             btns[i].onmouseenter=function(){
             for(let j=0;j<cons.length;j++){
-            btns[j].classList.remove("active3");
+            btns[j].classList.remove(".zhineng_top_wenzi_you[span=span1]");            
             cons[j].style.zIndex="";
         }
         cons[i].style.zIndex=10;        
@@ -112,35 +113,112 @@
     }    
     }
 }
-//为你推荐效果
-// {
-//     const prev=document.querySelector(".star_btn .prev1");
-//     const next=document.querySelector(".star_btn .next1");
-//     const inner=document.querySelector(".dapei_bottom_inner");
-//     let n=0;    //代表当前往左边移动几屏
-//     next.onclick=function(){
-//         n++;
-//         prev.classList.remove("disable");
-//         if(n===2){
-//             this.classList.add("disable");
-//         }
-//         if(n===3){
-//             n=2;
-//             return;  //终值函数执行
-//         }
-//         inner.style.marginLeft=-1240*n+"px";
-//     }
-//     prev.onclick=function(){
-//         n--;
-//         next.classList.remove("disable");
-//         if(n===0){
-//             this.classList.add("disable");
-//         }
-//         if(n===-1){
-//             n=0;
-//             return;
-//         }
-//         inner.style.marginLeft=-1240*n+"px";
-//     }
-
-// }
+// 为你推荐
+{
+    const prev=document.querySelector(".tuijian_danpin_anniu1");
+    const next=document.querySelector(".tuijian_danpin_anniu2");
+    const inner=document.querySelector(".tuijianxia_zong");
+    let n=0;//代表要往左边移动几屏
+    next.onclick=function () {
+        n++;
+        prev.classList.remove("disabled");
+        if(n===2){
+            this.classList.add("disabled");
+        }
+        if(n===3){
+            n=2;
+            return;
+        }
+        inner.style.marginLeft=-1240*n+"px";
+    };
+    prev.onclick=function () {
+        n--;
+        next.classList.remove("disabled");
+        if(n===0){
+            prev.classList.add("disabled");
+        }
+        if(n===-1){
+            n=0;
+            return;
+        }
+        inner.style.marginLeft=-1240*n+"px";
+    }
+}
+//内容
+{
+    function fengzhuang(parent){
+        let prev=parent.querySelector(".neirong_xia_prev");
+        let next=parent.querySelector(".neirong_xia_next");
+        let inner=parent.querySelector(".neirong_xia_inner");
+        let pager=parent.querySelectorAll(".neirong_item6_lunbo");
+        let n=0;
+        next.onclick=function () {
+            n++;
+            if(n===pager.length){
+                n=pager.length-1;
+                return;
+            }
+            inner.style.marginLeft=n*-296+"px";
+            pager[n].classList.add("neirong_item6_lunbo_active");
+            pager[n-1].classList.remove("neirong_item6_lunbo_active");
+            obj=pager[n];
+        };
+        prev.onclick=function () {
+            n--;
+            if(n<0){
+                n=0;
+                return;
+            }
+            inner.style.marginLeft=n*-296+"px";
+            pager[n].classList.add("neirong_item6_lunbo_active");
+            pager[n+1].classList.remove("neirong_item6_lunbo_active");
+            obj=pager[n];
+        };
+        let obj=pager[0];
+        pager.forEach(function (ele,index) {
+            ele.onclick=function () {
+                obj.classList.remove("neirong_item6_lunbo_active");
+                ele.classList.add("neirong_item6_lunbo_active");
+                obj=ele;
+                inner.style.marginLeft=index*-296+"px";
+                n=index;
+            }
+        })  
+    }
+   let contentList=document.querySelectorAll(".neirong_item");
+    contentList.forEach(function(a){
+        fengzhuang(a);
+    });
+}
+//侧导航
+{
+    let sidenav=document.querySelectorAll(".banner_cedaohang li");
+    let menu=document.querySelectorAll(".banner_cedaohang_chu");
+    let obj=menu[0];
+    sidenav.forEach(function(ele,index){
+        ele.onmouseenter=function(){
+            obj.style.display="none";
+            menu[index].style.display="block";
+            obj=menu[index];
+        }
+        ele.onmouseleave=function(){
+            menu[index].style.display="none";
+        }
+    })
+}
+//导航上文字
+{          
+    let text=document.querySelectorAll(".dh_wenzi_he");
+    let box=document.querySelectorAll(".dh_wenzi_chu");
+    let obj=box[0];
+    text.forEach(function (ele,index) {
+        ele.onmouseenter=function () {
+            obj.style.display="none";
+            box[index].style.display="block";
+            obj=box[index];
+        };
+        ele.onmouseleave=function () {
+            box[index].style.display="none";
+        };
+    })   
+}
